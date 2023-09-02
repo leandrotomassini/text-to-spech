29fb99c37bc725f4c9b6863d3ef97fb08195950c
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-09-02 16:40:12 -0300

    Optimizado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
-
-let voiceSelect = document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
-
-  voices.forEach(
-    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
-  );
+  speech.voice = voices[262];
 };
 
-voiceSelect.addEventListener("change", () => {
-  speech.voice = voices[voiceSelect.value];
-});
+speech.onend = function () {
+  console.log("La reproducción del audio ha terminado.");
+};
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
